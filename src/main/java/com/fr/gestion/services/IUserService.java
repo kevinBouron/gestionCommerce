899e588648a8c49840d90fd35940a2a6aa3567aa
JesscaLaot <jess.laot@gmail.com>
2019-05-01 17:20:50 +0200

    Premier commit

--- src/main/java/com/fr/gestion/services/IUserService.java
+++ src/main/java/com/fr/gestion/services/IUserService.java
@@ -1,5 +1,10 @@
 package com.fr.gestion.services;
 
-public interface IUserService {
+import org.springframework.stereotype.Service;
+
+import com.fr.gestion.DAO.IUserDao;
+
+
+public interface IUserService extends IUserDao {
 
 }
