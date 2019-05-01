67248314e9029e00c7b09635eabf994d7517e0ce
nhellou <nora.hellou@gmail.com>
2019-05-01 17:38:56 +0200

    commit nora

--- src/main/java/com/fr/gestion/DAO/IProduitDao.java
+++ src/main/java/com/fr/gestion/DAO/IProduitDao.java
@@ -1,5 +1,11 @@
 package com.fr.gestion.DAO;
 
-public interface IProduitDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.fr.gestion.entities.Produit;
+
+@RepositoryRestResource
+public interface IProduitDao extends JpaRepository<Produit, Long>{
 
 }
--- src/main/java/com/fr/gestion/entities/Produit.java
+++ src/main/java/com/fr/gestion/entities/Produit.java
@@ -1,5 +1,26 @@
 package com.fr.gestion.entities;
 
-public class Produit {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Produit implements Serializable{
+	@Id
+	@GeneratedValue
+	private long code;
+	private String modele;
+	private String fabricant;
+	private String couleur;
+	private float prix;
 
 }
