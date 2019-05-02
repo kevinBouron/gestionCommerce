086637d707abeb29493671c803af819b7ea9c91f
kevinBouron <kb.bouron@gmail.com>
2019-05-02 08:08:44 +0200

    kevin1

--- src/main/java/com/fr/gestion/entities/Facture.java
+++ src/main/java/com/fr/gestion/entities/Facture.java
@@ -1,31 +1,7 @@
 package com.fr.gestion.entities;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+public class Facture{
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-public class Facture implements Serializable{
-
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String modeReglement;
-	private float TVA;
-	private Date dateLivraison;
-	private String description;
-	private float montantPaye;
-	private int numPiece;
-	private Date dateEcheance;
 }
