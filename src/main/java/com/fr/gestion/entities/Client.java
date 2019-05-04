def5f3ccb9436f14874e5a2d580dfa03b98231bb
laurieannejouanno <laurieannejouanno@gmail.com>
2019-05-04 09:32:07 +0200

    Client et ICommandeDao

--- src/main/java/com/fr/gestion/entities/Client.java
+++ src/main/java/com/fr/gestion/entities/Client.java
@@ -1,5 +1,40 @@
 package com.fr.gestion.entities;
 
-public class Client {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Client implements Serializable {
+
+	
+	@Id
+	@GeneratedValue
+	private Long codeClient;
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private String adresse;
+	private String nomSociete;
+	private String telMobile;
+	private String telFixe;
+	private String fax;
+	private String email;
+	
+	@OneToMany
+	private List<Commande> commandes;
+
 
 }
