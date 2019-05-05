f70bb5f91f83c70944d8324e88f4c82946b6dee4
Marine Le Cleach <49683302+MarineLC@users.noreply.github.com>
2019-05-05 16:41:47 +0200

    brancheM

--- src/main/java/com/fr/gestion/services/CommandeService.java
+++ src/main/java/com/fr/gestion/services/CommandeService.java
@@ -1,5 +1,47 @@
 package com.fr.gestion.services;
 
-public class CommandeService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fr.gestion.DAO.ICommandeDao;
+import com.fr.gestion.entities.Commande;
+
+
+
+@Service
+public class CommandeService implements ICommandeService{
+
+	
+	
+	@Autowired
+	private ICommandeDao commandeDao;
+
+	public Commande findOneById(Long id) {
+		// TODO Auto-generated method stub
+		return commandeDao.getOne(id);
+	}
+
+
+	public Commande save(Commande c) {
+		// TODO Auto-generated method stub
+		return commandeDao.save(c);
+	}
+
+	public void delete(Commande c) {
+		// TODO Auto-generated method stub
+		commandeDao.delete(c);
+	}
+
+
+	public List<Commande> getAll() {
+		// TODO Auto-generated method stub
+		return commandeDao.findAll();
+	}
+	
+
+	
+	
 
 }
